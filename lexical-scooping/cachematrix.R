eef8bb7b2398a15768fb9fb65d285557345849f1
Dora Simroth <d4dorai@users.noreply.github.com>
2014-08-14 11:24:39 +0200

    Create cachematrix.R

---
+++ lexical-scooping/cachematrix.R
@@ -0,0 +1,35 @@
+##Here I calculate the inverse of a matrix using a Cache if available
+## This creates a "special" matrix --> a list!
+makeCacheMatrix <- function(x = matrix()) {
+  
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
+}
+
+
+## this calculates the inverse of the special matrix
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
